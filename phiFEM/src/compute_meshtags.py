Fixes the computation of Omega_h boundary facets with padding cells.

phiFEM/src/compute_meshtags.py
@@ -77,7 +77,7 @@ def tag_entities(mesh,
         interior_entities     = cells_tags.indices[np.where(cells_tags.values == 1)]
         cut_fronteer_entities = cells_tags.indices[np.where(cells_tags.values == 2)]
         exterior_entities     = cells_tags.indices[np.where(cells_tags.values == 3)]
-
+        padding_entities      = cells_tags.indices[np.where(cells_tags.values == 4)]
     if edim == mesh.topology.dim - 1:
         # Get the indices of facets belonging to cells in interior_entities and cut_fronteer_entities
         mesh.topology.create_connectivity(cdim, edim)
@@ -88,7 +88,7 @@ def tag_entities(mesh,
         interior_boundary_facets = np.intersect1d(c2f_map[interior_entities],
                                                   c2f_map[cut_fronteer_entities])
         boundary_facets = np.intersect1d(c2f_map[cut_fronteer_entities], 
-                                         c2f_map[exterior_entities])
+                                         np.union1d(c2f_map[exterior_entities], c2f_map[padding_entities]))
 
         interior_fronteer_facets, cut_facets, exterior_facets, _ = _select_entities(mesh, levelset, edim)
         interior_facets = np.setdiff1d(interior_fronteer_facets, interior_boundary_facets)
@@ -107,11 +107,20 @@ def tag_entities(mesh,
                  cut_facets,
                  exterior_facets,
                  boundary_facets]
+        
+        assert len(interior_facets) > 0, "No interior facets (1) tagged!"
+        assert len(cut_facets)      > 0, "No cut facets (2) tagged!"
+        assert len(boundary_facets) > 0, "No boundary facets (4) tagged!"
     elif edim == mesh.topology.dim:
         lists = [interior_entities,
                  cut_fronteer_entities,
                  exterior_entities,
                  padding_entities]
+
+        assert len(interior_entities)     > 0,  "No interior cells (1) tagged!"
+        assert len(cut_fronteer_entities) > 0,  "No cut cells (2) tagged!"
+        if padding:
+            assert len(padding_entities)  > 0, "No padding cells (4) tagged!"
 
     list_markers = [np.full_like(l, i+1) for i, l in enumerate(lists)]
     entities_indices = np.hstack(lists).astype(np.int32)

phiFEM/src/solver.py
@@ -90,6 +90,7 @@ class PhiFEMSolver:
         # Tag cells of the background mesh. Used to tag the facets and/or create the submesh.
         self.bg_mesh_cells_tags = tag_entities(self.bg_mesh, self.levelset, self.bg_mesh.topology.dim, padding=padding)
         working_cells_tags = self.bg_mesh_cells_tags
+
         # Create the submesh and transfer the cells tags from the bg mesh to the submesh.
         # Tag the facets of the submesh.
         omega_h_cells = self.bg_mesh_cells_tags.indices[np.where(np.logical_or(np.logical_or(self.bg_mesh_cells_tags.values == 1, self.bg_mesh_cells_tags.values == 2), self.bg_mesh_cells_tags.values == 4))]
@@ -101,6 +102,7 @@ class PhiFEMSolver:
         self.facets_tags = tag_entities(working_mesh, self.levelset, working_mesh.topology.dim - 1, cells_tags=working_cells_tags)
 
         if plot:
+            print("WARNING: We plot the tags for the cells and facets: might drastically slow the refinement loop!")
             figure, ax = plt.subplots()
             plot_mesh_tags(working_mesh, self.facets_tags, ax=ax, display_indices=False, expression_levelset=self.levelset.expression)
             plt.savefig(f"./output_phiFEM/test_facets_{str(self.i).zfill(2)}.svg", format="svg", dpi=2400)
@@ -274,8 +276,8 @@ class PhiFEMSolver:
         """
         L2 estimator
         """
-        eta_T = h_T**4 * inner(inner(r, r), w0) * dx
-        eta_E = avg(h_E)**3 * inner(inner(J_h, J_h), avg(w0)) * dS
+        eta_T = h_T**4 * inner(inner(r, r), w0) * (dx(1) + dx(2))
+        eta_E = avg(h_E)**3 * inner(inner(J_h, J_h), avg(w0)) * (dS(1) + dS(2))
 
         eta = eta_T + eta_E
 