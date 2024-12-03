47c223fc77f8864759166a07c091af3d91f9a47b
rbulle <rbulle@tutanota.com>
2024-12-03 14:41:30 +0100

    Add new directories from refactoring. Add mesh2d_from_levelset test.

---
+++ phiFEM/src/compute_meshtags.py
@@ -0,0 +1,126 @@
+import dolfinx as dfx
+from dolfinx import cpp
+import numpy as np
+
+# TODO: Modify to use in parallel
+
+def _select_entities(mesh, levelset, edim, padding=False):
+    """ Compute the list of entities strictly inside Omega_h and the list of entities having a non-empty intersection with Gamma_h.
+
+    Args:
+        mesh: the background mesh.
+        levelset: a Levelset object
+        edim: the dimension of the entities.
+    
+    Returns:
+        Two lists of entities indices.
+    """
+    mesh.topology.create_connectivity(edim, edim)
+    # List all entities of the mesh
+    entities = np.arange(mesh.topology.index_map(edim).size_global, dtype = np.int32) # TODO: change this line to allow parallel computing
+
+    # List entities that are stricly included in Omega_h
+    list_interior_entities = dfx.mesh.locate_entities(mesh,
+                                                      edim,
+                                                      levelset.interior(0.))
+
+    # List entities that are strictly excluded from Omega_h
+    exterior_entities = dfx.mesh.locate_entities(mesh,
+                                                 edim,
+                                                 levelset.exterior(0.))
+
+    # List entities having a non-empty intersection with Omega_h
+    interior_entities = np.setdiff1d(entities, exterior_entities)
+    # Cells case
+    # List entities having a non-empty intersection with Gamma_h
+    cut_entities = np.setdiff1d(interior_entities, list_interior_entities)
+
+    padding_entities = []
+    if padding:
+        hmax_cutcells = max(cpp.mesh.h(mesh._cpp_object, edim, cut_entities))
+        list_exterior_padding_entities = dfx.mesh.locate_entities(mesh,
+                                                                  edim,
+                                                                  levelset.exterior(0., padding=hmax_cutcells))
+        padding_interior_entities = np.setdiff1d(entities, list_exterior_padding_entities)
+        padding_entities = np.setdiff1d(padding_interior_entities, np.union1d(interior_entities, cut_entities))
+        exterior_entities = np.setdiff1d(exterior_entities, padding_entities)
+
+    return list_interior_entities, cut_entities, exterior_entities, padding_entities
+
+def tag_entities(mesh,
+                 levelset,
+                 edim,
+                 cells_tags=None,
+                 padding=False):
+    """ Compute the entity tags for the interior (Omega_h) and the cut (set of cells having a non empty intersection with Gamma_h).
+    Tag = 1: interior strict (Omega_h \ Omega_Gamma_h)
+    Tag = 2: cut (Omega_Gamma_h)
+    Tag = 3: exterior (entities strictly outside Omega_h)
+    Tag = 4: facets: Gamma_h
+             cells: padding cells.
+
+    Args:
+        mesh: the background mesh.
+        levelset: a Levelset object.
+        edim: dimension of the entities.
+        cells_tags (Meshtags object): the cells tags (default: None).
+
+    Returns:
+        A meshtags object.
+    """
+
+    # If cells_tags is not provided, we need to go through cells selection.
+    cdim = mesh.topology.dim
+    if cells_tags is None:
+        interior_entities, cut_fronteer_entities, exterior_entities, padding_entities = _select_entities(mesh, levelset, cdim, padding=padding)
+    else:
+        interior_entities     = cells_tags.indices[np.where(cells_tags.values == 1)]
+        cut_fronteer_entities = cells_tags.indices[np.where(cells_tags.values == 2)]
+        exterior_entities     = cells_tags.indices[np.where(cells_tags.values == 3)]
+
+    if edim == mesh.topology.dim - 1:
+        # Get the indices of facets belonging to cells in interior_entities and cut_fronteer_entities
+        mesh.topology.create_connectivity(cdim, edim)
+        c2f_connect = mesh.topology.connectivity(cdim, edim)
+        num_facets_per_cell = len(c2f_connect.links(0))
+        # Note: here the reshape does not need any special treatment since all cells have the same number of facets.
+        c2f_map = np.reshape(c2f_connect.array, (-1, num_facets_per_cell))
+        interior_boundary_facets = np.intersect1d(c2f_map[interior_entities],
+                                                  c2f_map[cut_fronteer_entities])
+        boundary_facets = np.intersect1d(c2f_map[cut_fronteer_entities], 
+                                         c2f_map[exterior_entities])
+
+        interior_fronteer_facets, cut_facets, exterior_facets, _ = _select_entities(mesh, levelset, edim)
+        interior_facets = np.setdiff1d(interior_fronteer_facets, interior_boundary_facets)
+        cut_facets = np.union1d(cut_facets, interior_boundary_facets)
+        exterior_facets = np.setdiff1d(exterior_facets, boundary_facets)
+        
+        # Add the boundary facets to the proper lists of facets (shouldn't be necessary since the intersection of Gamma_h with the boundary of the box should be empty)
+        background_boundary_facets = dfx.mesh.locate_entities_boundary(mesh,
+                                                                       edim,
+                                                                       lambda x: np.ones_like(x[1]).astype(bool))
+        intersect_exterior_boundary_facets = np.intersect1d(c2f_map[cut_fronteer_entities], background_boundary_facets)
+        exterior_facets = np.setdiff1d(exterior_facets, intersect_exterior_boundary_facets)
+        boundary_facets = np.union1d(boundary_facets, intersect_exterior_boundary_facets)
+
+        lists = [interior_facets,
+                 cut_facets,
+                 exterior_facets,
+                 boundary_facets]
+    elif edim == mesh.topology.dim:
+        lists = [interior_entities,
+                 cut_fronteer_entities,
+                 exterior_entities,
+                 padding_entities]
+
+    list_markers = [np.full_like(l, i+1) for i, l in enumerate(lists)]
+    entities_indices = np.hstack(lists).astype(np.int32)
+    entities_markers = np.hstack(list_markers).astype(np.int32)
+
+    sorted_indices = np.argsort(entities_indices)
+
+    entities_tags = dfx.mesh.meshtags(mesh,
+                                      edim,
+                                      entities_indices[sorted_indices],
+                                      entities_markers[sorted_indices])
+    return entities_tags
---
+++ phiFEM/src/mesh_scripts.py
@@ -0,0 +1,215 @@
+# Snippet stolen from https://github.com/multiphenics/multiphenicsx/blob/main/tutorials/07_understanding_restrictions/tutorial_understanding_restrictions.ipynb...
+# ...and butchered so that we can pass a mesh_tags with more than 2 different tags.
+# TODO: add more line styles for the moment it's not very colorblind friendly.
+
+from basix.ufl import element
+import dolfinx as dfx
+import matplotlib as mpl
+from matplotlib import colormaps as cm
+import mpl_toolkits.axes_grid1
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import meshio
+import numpy as np
+import os
+import pygmsh
+import ufl
+from ufl import inner, grad
+
+def mesh2d_from_levelset(lc, levelset, level=0., bbox=np.array([[-1., 1.], [-1., 1.]]), geom_vertices=None, output_dir=None):
+    """ Generate a 2D conforming mesh from a levelset function and saves it as an xdmf mesh.
+
+    Args:
+        lc:            characteristic length of the mesh.
+        levelset:      levelset function (as a Levelset object).
+        level:         the level of the isoline (default: 0.).
+        bbox:          bounding box of the isoline (default: np.array([[-1., 1.], [-1., 1.]])).
+        geom_vertices: specific vertices to be added to the isoline (e.g. vertices of the geometry.)
+        output_dir:    directory path where the mesh is saved. If None the mesh is not saved.
+
+    Returns:
+        The coordinates of the boundary vertices.
+    """
+
+    x = np.arange(bbox[0,0], bbox[0,1], step=lc/np.sqrt(2.), dtype=np.float64)
+    y = np.arange(bbox[1,0], bbox[1,1], step=lc/np.sqrt(2.), dtype=np.float64)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+    X_flat, Y_flat = X.flatten(), Y.flatten()
+    Z_flat = levelset(X_flat, Y_flat)
+    Z = np.reshape(Z_flat, X.shape)
+
+    c = plt.contour(X, Y, Z, [level])
+    boundary_vertices = c.collections[0].get_paths()[0].vertices
+    if geom_vertices is not None:
+        boundary_vertices = np.vstack(geom_vertices)
+    
+    with pygmsh.geo.Geometry() as geom:
+        # The boundary vertices are correctly ordered by matplotlib.
+        geom.add_polygon(boundary_vertices, mesh_size=lc)
+        mesh = geom.generate_mesh(dim=2)
+
+    for cell_block in mesh.cells:
+        if cell_block.type == "triangle":
+            triangular_cells = [("triangle", cell_block.data)]
+
+    if output_dir is not None:
+        meshio.write_points_cells(os.path.join(output_dir, "conforming_mesh.xdmf"), mesh.points, triangular_cells)
+    return boundary_vertices
+
+def compute_outward_normal(mesh, mesh_tags, levelset):
+    # This function is used to define the unit outward pointing normal to Gamma_h
+    CG1Element = element("CG", mesh.topology.cell_name(), 1)
+    V = dfx.fem.functionspace(mesh, CG1Element)
+    DG0VecElement = element("DG", mesh.topology.cell_name(), 0, shape=(mesh.topology.dim,))
+    W0 = dfx.fem.functionspace(mesh, DG0VecElement)
+    ext = dfx.fem.Function(V)
+    ext.interpolate(levelset.exterior(0.))
+
+    # Compute the unit outwards normal, but the scaling might create NaN where grad(ext) = 0
+    normal_Omega_h = grad(ext) / (ufl.sqrt(inner(grad(ext), grad(ext))))
+
+    # In order to remove the eventual NaNs, we interpolate into a vector functions space and enforce the values of the gradient to 0. in the cells that are not cut
+    w0 = dfx.fem.Function(W0)
+    w0.sub(0).interpolate(dfx.fem.Expression(normal_Omega_h[0], W0.sub(0).element.interpolation_points()))
+    w0.sub(1).interpolate(dfx.fem.Expression(normal_Omega_h[1], W0.sub(1).element.interpolation_points()))
+
+    w0.sub(0).x.array[:] = np.nan_to_num(w0.sub(0).x.array, nan=0.0)
+    w0.sub(1).x.array[:] = np.nan_to_num(w0.sub(1).x.array, nan=0.0)
+    return w0
+
+def reshape_facets_map(f2c_connect):
+    f2c_array = f2c_connect.array
+    num_cells_per_facet = np.diff(f2c_connect.offsets)
+    max_cells_per_facet = num_cells_per_facet.max()
+    f2c_map = -np.ones((len(f2c_connect.offsets) - 1, max_cells_per_facet), dtype=int)
+
+    # Mask to select the boundary facets
+    mask = np.where(num_cells_per_facet == 1)
+    f2c_map[mask, 0] = f2c_array[num_cells_per_facet.cumsum()[mask] - 1]
+    f2c_map[mask, 1] = f2c_array[num_cells_per_facet.cumsum()[mask] - 1]
+    # Mask to select the interior facets
+    mask = np.where(num_cells_per_facet == 2)
+    f2c_map[mask, 0] = f2c_array[num_cells_per_facet.cumsum()[mask] - 2]
+    f2c_map[mask, 1] = f2c_array[num_cells_per_facet.cumsum()[mask] - 1]
+    return f2c_map
+
+def msh2xdmf_conversion_2D(msh_file_path, cell_type, prune_z=False):
+    mesh = meshio.read(msh_file_path)
+    cells = mesh.get_cells_type(cell_type)
+    cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+    points = mesh.points[:, :2] if prune_z else mesh.points
+    triangle_mesh = meshio.Mesh(points=points,
+                                cells={cell_type: cells},
+                                cell_data={"name_to_read": [cell_data.astype(np.int32)]})
+    path, name_ext = os.path.split(msh_file_path)
+    name = os.path.splitext(name_ext)[0]
+    meshio.write(os.path.join(path, name, ".xdmf"), triangle_mesh)
+
+def plot_mesh_tags(mesh, mesh_tags, ax = None, display_indices=False, expression_levelset=None):
+    """Plot a mesh tags object on the provied (or, if None, the current) axes object."""
+    if ax is None:
+        ax = plt.gca()
+    ax.set_aspect("equal")
+    points = mesh.geometry.x
+    cmap = [cm.get_cmap('tab10')(i / 10.) for i in range(10)]
+    cmap_bounds = np.arange(10)
+    norm = mpl.colors.BoundaryNorm(cmap_bounds, len(cmap))
+    assert mesh_tags.dim in (mesh.topology.dim, mesh.topology.dim - 1)
+    cells_map = mesh.topology.index_map(mesh.topology.dim)
+    num_cells = cells_map.size_local + cells_map.num_ghosts
+    if mesh_tags.dim == mesh.topology.dim:
+        cells = mesh.geometry.dofmap
+        tria = tri.Triangulation(points[:, 0], points[:, 1], cells)
+        cell_colors = np.zeros((cells.shape[0], ))
+        if display_indices:
+            tdim = mesh.topology.dim
+            connectivity_cells_to_vertices = mesh.topology.connectivity(tdim, 0)
+            vertex_map = {
+                topology_index: geometry_index for c in range(num_cells) for (topology_index, geometry_index) in zip(
+                    connectivity_cells_to_vertices.links(c), mesh.geometry.dofmap[c])
+            }
+        for c in range(num_cells):
+            if c in mesh_tags.indices:
+                cell_colors[c] = mesh_tags.values[np.where(mesh_tags.indices == c)][0]
+                if display_indices:
+                    vertices = [vertex_map[v] for v in connectivity_cells_to_vertices.links(c)]
+                    midpoint = np.sum(points[vertices], axis=0)/np.shape(points[vertices])[0]
+                    ax.text(midpoint[0], midpoint[1], f"{c}", horizontalalignment="center", verticalalignment="center", fontsize=6)
+            else:
+                cell_colors[c] = 0
+        # cell_colors[mesh_tags.indices[mesh_tags.values != 0]] = 1
+        mappable: mpl.collections.Collection = ax.tripcolor(
+            tria, cell_colors, edgecolor="k", cmap="tab10", norm=norm)
+    elif mesh_tags.dim == mesh.topology.dim - 1:
+        tdim = mesh.topology.dim
+        connectivity_cells_to_facets = mesh.topology.connectivity(tdim, tdim - 1)
+        connectivity_cells_to_vertices = mesh.topology.connectivity(tdim, 0)
+        connectivity_facets_to_vertices = mesh.topology.connectivity(tdim - 1, 0)
+        vertex_map = {
+            topology_index: geometry_index for c in range(num_cells) for (topology_index, geometry_index) in zip(
+                connectivity_cells_to_vertices.links(c), mesh.geometry.dofmap[c])
+        }
+        lines = list()
+        lines_colors_as_int = list()
+        lines_colors_as_str = list()
+        lines_linestyles = list()
+        for c in range(num_cells):
+            facets = connectivity_cells_to_facets.links(c)
+            for f in facets:
+                if f in mesh_tags.indices:
+                    value_f = mesh_tags.values[np.where(mesh_tags.indices == f)][0]
+                else:
+                    value_f = 0
+                vertices = [vertex_map[v] for v in connectivity_facets_to_vertices.links(f)]
+                lines.append(points[vertices][:, :2])
+                lines_colors_as_int.append(value_f)
+                lines_colors_as_str.append(cmap[value_f])
+                lines_linestyles.append("solid")
+                if display_indices:
+                    midpoint = np.sum(points[vertices], axis=0)/np.shape(points[vertices])[0]
+                    ax.text(midpoint[0], midpoint[1], f"{f}", horizontalalignment="center", verticalalignment="center", fontsize=6)
+        mappable: mpl.collections.Collection = mpl.collections.LineCollection(  # type: ignore[no-redef]
+            lines, cmap="tab10", norm=norm, colors=lines_colors_as_str, linestyles=lines_linestyles, linewidth=0.2)
+        mappable.set_array(np.array(lines_colors_as_int))
+        ax.add_collection(mappable)
+        ax.autoscale()
+    divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(mappable, cax=cax, boundaries=cmap_bounds, ticks=cmap_bounds)
+
+    if expression_levelset is not None:
+        x_min, x_max = np.min(points[:,0]), np.max(points[:,0])
+        y_min, y_max = np.min(points[:,1]), np.max(points[:,1])
+        nx = 1000
+        ny = 1000
+        xs = np.linspace(x_min, x_max, nx)
+        ys = np.linspace(y_min, y_max, ny)
+
+        xx, yy = np.meshgrid(xs, ys)
+        xx_rs = xx.reshape(xx.shape[0] * xx.shape[1])
+        yy_rs = yy.reshape(yy.shape[0] * yy.shape[1])
+        zz_rs = expression_levelset(xx_rs, yy_rs)
+        zz = zz_rs.reshape(xx.shape)
+
+        ax.contour(xx, yy, zz, [0.], linewidths=0.2)
+    return ax
+
+def compute_facets_to_refine(mesh, facets_tags):
+    cdim = mesh.topology.dim
+    fdim = mesh.topology.dim - 1
+    Gamma_h_facets = facets_tags.indices[np.where(facets_tags.values == 4)]
+
+    mesh.topology.create_connectivity(fdim, cdim)
+    mesh.topology.create_connectivity(cdim, fdim)
+
+    f2c_connect = mesh.topology.connectivity(fdim, cdim)
+    c2f_connect = mesh.topology.connectivity(cdim, fdim)
+
+    num_facets_per_cell = len(c2f_connect.links(0))
+
+    c2f_map = np.reshape(c2f_connect.array, (-1, num_facets_per_cell))
+    f2c_map = reshape_facets_map(f2c_connect)
+    cells_connected_to_Gamma_h = np.unique(np.argsort(np.ndarray.flatten(f2c_map[Gamma_h_facets])))
+    facets_connected_to_Gamma_h = np.unique(np.argsort(np.ndarray.flatten(c2f_map[cells_connected_to_Gamma_h])))
+    facets_Omega_h = facets_tags.indices[np.where(np.logical_or(facets_tags.values == 1, facets_tags.values == 2, facets_tags.values == 4))]
+    return np.union1d(facets_Omega_h, facets_connected_to_Gamma_h)
