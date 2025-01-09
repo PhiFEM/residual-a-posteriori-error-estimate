Fix issue with boundary vertices shape and fix rotation in test and lshaped demo.

demo/lshaped/main.py
@@ -9,11 +9,16 @@ tilt_angle = np.pi/6.
 shift = np.array([np.pi/32., np.pi/32.])
 
 def rotation(angle, x):
-    # Rotation matrix
-    R = np.array([[ np.cos(angle), np.sin(angle)],
-                  [-np.sin(angle), np.cos(angle)]])
-    rotated = R.dot(np.array(x))
-    return rotated
+    if x.shape[0] == 3:
+        R = np.array([[np.cos(angle), -np.sin(angle), 0],
+                       [np.sin(angle),  np.cos(angle), 0],
+                       [             0,               0, 1]])
+    elif x.shape[0] == 2:
+        R = np.array([[np.cos(angle), -np.sin(angle)],
+                       [np.sin(angle),  np.cos(angle)]])
+    else:
+        raise ValueError("Incompatible argument dimension.")
+    return R.dot(np.asarray(x))
 
 def line(x, y, a, b, c):
     rotated = rotation(tilt_angle, [x, y])
@@ -70,12 +75,12 @@ if __name__=="__main__":
                                  compute_exact_error=compute_exact_errors)
     
     if solver=="FEM":
-        point_1 = rotation(- tilt_angle, np.array([0.,  0.]) * 0.5) + shift
-        point_2 = rotation(- tilt_angle, np.array([0., -1.]) * 0.5) + shift
-        point_3 = rotation(- tilt_angle, np.array([1., -1.]) * 0.5) + shift
-        point_4 = rotation(- tilt_angle, np.array([1.,  1.]) * 0.5) + shift
-        point_5 = rotation(- tilt_angle, np.array([-1., 1.]) * 0.5) + shift
-        point_6 = rotation(- tilt_angle, np.array([-1., 0.]) * 0.5) + shift
+        point_1 = rotation(tilt_angle, np.array([0.,  0.]) * 0.5) + shift
+        point_2 = rotation(tilt_angle, np.array([0., -1.]) * 0.5) + shift
+        point_3 = rotation(tilt_angle, np.array([1., -1.]) * 0.5) + shift
+        point_4 = rotation(tilt_angle, np.array([1.,  1.]) * 0.5) + shift
+        point_5 = rotation(tilt_angle, np.array([-1., 1.]) * 0.5) + shift
+        point_6 = rotation(tilt_angle, np.array([-1., 0.]) * 0.5) + shift
 
         geom_vertices = np.vstack([point_1, point_2, point_3, point_4, point_5, point_6]).T
         poisson_dirichlet_FEM(cl,

phiFEM/phifem/mesh_scripts.py
@@ -60,16 +60,18 @@ def mesh2d_from_levelset(lc: float,
         Z_flat = levelset(arr)
         Z = np.reshape(Z_flat, X.shape)
         cg = contour_generator(x=X, y=Y, z=Z, name="threaded")
-        boundary_vertices = cast(npt.NDArray[np.float64], cg.lines(0.)[0])
+        boundary_vertices = cast(npt.NDArray[np.float64], cg.lines(0.)[0]).T
     else:
-        if geom_vertices.shape[0] == 1:
-            boundary_vertices = np.vstack((geom_vertices, np.zeros_like(geom_vertices), np.zeros_like(geom_vertices)))
-        elif geom_vertices.shape[0] == 2:
-            boundary_vertices = np.vstack((geom_vertices, np.zeros_like(geom_vertices[0, :])))
-        elif geom_vertices.shape[0] == 3:
-            boundary_vertices = geom_vertices
-        else:
-            raise ValueError("The geometry vertices must have at most 3 coordinates, not more.")
+        boundary_vertices = geom_vertices
+    
+    if boundary_vertices.shape[0] == 1:
+        boundary_vertices = np.vstack((boundary_vertices, np.zeros_like(boundary_vertices), np.zeros_like(boundary_vertices)))
+    elif boundary_vertices.shape[0] == 2:
+        boundary_vertices = np.vstack((boundary_vertices, np.zeros_like(boundary_vertices[0, :])))
+    elif boundary_vertices.shape[0] == 3:
+        boundary_vertices = boundary_vertices
+    else:
+        raise ValueError("The geometry vertices must have at most 3 coordinates, not more.")
     
     with pygmsh.geo.Geometry() as geom:
         # The boundary vertices are correctly ordered by matplotlib.
@@ -95,7 +97,7 @@ def mesh2d_from_levelset(lc: float,
         
         tree.write(os.path.join(output_dir, f"{file_name}.xdmf"), pretty_print=True, xml_declaration=True, encoding="UTF-8")
     
-    return boundary_vertices.T
+    return boundary_vertices
 
 def compute_outward_normal(mesh: Mesh, levelset: Levelset) -> Function:
     """ Compute the outward normal to Omega_h.